7a68aa276ad3e50c04aee318cf9457a051fee99b
Maya Iuga <maya_iuga@yahoo.ro>
2022-06-16 19:07:36 +0100

    Adds methods descriptions to new GUI

--- GUI/webscraper_GUI.py
+++ GUI/webscraper_GUI.py
@@ -5,68 +5,80 @@ import tkinter
 from PIL import ImageTk, Image
 import requests
 from project_module_2_GUI_version import Run_Scraper
-import tkinter.messagebox
+import tkinter
+
 
 class MainFrame(tk.Tk):
 
-    """frame object holding all of our different pages
-    controller of the pages
-    """
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
+    """Frame object/Controller holding all of our different pages"""
 
+    def __init__(self, *args, **kwargs):
+
+        """See help(MainFrame) for more details"""
+
+        tk.Tk.__init__(self, *args, **kwargs)
         self.titlefont = tkfont.Font(family = 'Helvetica', size = 18, weight = 'bold', slant = 'roman')
         self.title('Book Scraper')
         self.geometry('600x400')
       
-        # frame object that will hold all the pages in
+        # frame object that will hold all the pages 
         container = tk.Frame(self)
         container.pack(side = "top", fill = "both")
-        #nesw stretch all cardinal directions
-        #container.grid(row = 0, column = 0, sticky = 'nesw')
         container.grid_rowconfigure(0, weight = 1)
         container.columnconfigure (0, weight = 1)
 
-        # create empty dictionary to define all pages later on
+        # create empty dictionary to define all pages
         self.listing = {}
-        # will build these pages later on as classes
+        # These pages are built later on as classes
         for page in (WelcomePage, PageOne, PageTwo):
             page_name = page.__name__
             # new frame object for each page
             frame = page(parent = container, controller = self)
             # same as main frame - completely layered without distinction
             frame.grid(row = 0, column = 0, sticky = 'nesw')
-            
             self.listing[page_name] = frame
 
         self.up_frame('WelcomePage')
         
-    # define first page to pop up/raise
+    # define first page to pop up
     def up_frame(self, page_name):
         page = self.listing[page_name]
         page.tkraise()
 
+
 class WelcomePage(tk.Frame):
 
+    """Frame object corresponding to the first/welcome page of the GUI"""
+
     def __init__(self, parent, controller):
+
+        """See help(WelcomePage) for more details"""
+
         tk.Frame.__init__(self, parent)
         self.controller = controller
-             
+
+        # set image as background of frame     
         self.background_image = ImageTk.PhotoImage(file=r'book.png')
         self.background_label = tk.Label(self, image = self.background_image)
         self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
         self.background_label.image = self.background_image
 
+        # add label
         label = tk.Label(self, text = 'Waterstones Web Scraping Tool', font = controller.titlefont)
         label.pack(pady = 100)
-
+        # add Get Started button
         button = tk.Button(self, text = 'Get Started', command = lambda: controller.up_frame("PageOne"))
         button.pack(pady = 75)
 
 
 class PageOne(tk.Frame):
+    
+    """Frame object corresponding to page one of the GUI"""
 
     def __init__(self, parent, controller):
+
+        """See help(PageOne) for more details"""
+
         tk.Frame.__init__(self, parent, bg = "#27408B")
         self.controller = controller
 
@@ -75,11 +87,12 @@ class PageOne(tk.Frame):
         bg="#27408B", fg="#FFF5EE", height = '5', width='54',font=("Helvetica 18 italic bold"))
         header.grid(row=0, column=0)
 
+        # adds the drop down menus
         drop_down_menus = self.initialise_menus()
         drop_down_menus[0].grid(row=1, column=0, sticky=W, pady=30)
         drop_down_menus[1].grid(row=1, column=0, pady=30)
         drop_down_menus[2].grid(row=1, column=0, sticky=E, pady=30, padx=20)
-        #
+        # adds the user entry variables
         self.intialises_user_entries()
         number_pages = tk.Label(self, text = "NUMBER OF PAGES:",  height = '5', width='15', bg="#27408B", fg="#F5F5F5")
         number_pages.grid(column=0, row=2, sticky=W, padx = 50, pady = 5)
@@ -88,16 +101,20 @@ class PageOne(tk.Frame):
         postcode.grid(row=2, column=0, sticky=E, padx = 150, pady = 5)
         self.postcode_entry.grid(row =2, column=0, sticky=E, padx = 60, pady = 5)
 
+        # Start button will start the scraper & once run will navigate to final page
         start_button = tk.Button(self, text = "Start", command = lambda: [controller.up_frame("PageTwo"), self.run_app()], fg = "#548B54",
         relief = "raised", width = 12, height = 3, font = ("Helvetica 12 bold"))
         start_button.grid(column = 0, row = 3, sticky = 'e',padx = 65, pady = 2)
-
+        # Back button to return to welcome page
         back_button = tk.Button(self, text = "Back", command = lambda: controller.up_frame("WelcomePage"), fg = "#CD5B45",
         relief = "raised", width = 12, height = 3, font = ("Helvetica 12 bold"))
         back_button.grid(column = 0, row = 3, sticky = 'w', padx = 65, pady = 2)
 
 
     def user_input(self):
+
+        """This methods allows to easily monitor changes of tinker variables"""
+
         self.category_menu = tk.StringVar()
         self.category_menu.set("Select Any Category")
         self.subcategory_menu = tk.StringVar()
@@ -109,6 +126,8 @@ class PageOne(tk.Frame):
 
 
     def initialise_menus(self):
+
+        """This methods initialises the options of the drop-down menus"""
         
         self.user_input()
         drop_category = tk.OptionMenu(self, self.category_menu, "fiction", "crime-thrillers-mystery", "science-fiction-fantasy-horror","graphic-novels-manga", "non-fiction-books")
@@ -126,31 +145,37 @@ class PageOne(tk.Frame):
 
         return drop_category, drop_subcategory, drop_headless
 
+
     def intialises_user_entries(self):
+
+        """This method initialises user entries &
+        applies pre-defined functions on them"""
 
         self.number_pages_entry = tk.Entry(self, textvariable = self.number_pages, width = 3, bd=2)
         self.postcode_entry = tk.Entry(self, textvariable = self.postcode, width = 8, bd =2)
-        #call pre-defined functions on postcode input
+        #call functions on postcode input
         self.postcode_entry.bind("<KeyRelease>", self.caps_postcode)
         self.postcode_entry.bind("<Leave>", self.check_postcode)
         
 
     def caps_postcode(self, event):
 
-        """ This function capitalises every letter in the postcode"""
+        """ This function capitalises every letter in the postcode entry"""
 
         self.postcode.set(self.postcode.get().upper())
+
 
     def check_postcode(self, event):
 
         """ This function will check postcode input corresponds to a 
-        real postcode, otherwise it will randomly generate a new one."""
+        real postcode, otherwise it will raise a pop up message."""
 
         postcode_check = self.postcode.get()
         postcode_status = requests.get('https://api.postcodes.io/postcodes/' + postcode_check).json()["status"]
         if postcode_status == 404:
             #self.postcodepostcode.set(requests.get('https://api.postcodes.io/random/postcodes').json()["result"]["postcode"])
             tkinter.messagebox.showinfo("Error Message", "Not a valid UK postcode, try again!")
+
 
     def run_app(self):
 
@@ -173,34 +198,47 @@ class PageOne(tk.Frame):
         
 class PageTwo(tk.Frame):
 
+    """Frame object corresponding to page two of the GUI"""
+
     def __init__(self, parent, controller):
+
+        """See help(PageOne) for more details"""
+
         tk.Frame.__init__(self, parent, bg = "#27408B")
         self.controller = controller
-
+        
+        # opens every image frame in the gif
         info = Image.open('book.gif')
         self.frames = info.n_frames
         self.im = [tk.PhotoImage(file='book.gif',format=f"gif -index {i}") for i in range(self.frames)]
         count = 0
         anim = None
-
+        
+        # sets background of the tkinter frame
         self.background_label = tk.Label(self,image="")
         self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
         self.background_label.image = self.im
-        
+        # loops through each image frame
         self.animation(count)
 
+        # display message
         label = tk.Label(self, text = 'Everything went well!', font = controller.titlefont)
         label.pack(pady = 50)
-
+        # Exit button to quit the GUI
         exit_button = tk.Button(self, text = 'Exit', command = lambda: controller.destroy())
         exit_button.pack(pady = 125)
 
+
     def animation(self, count):
+
+        """This function will set each of the image frames as the
+        frame background one by one 50ms apart"""
+
         global anim
         im2 = self.im[count]
-
         self.background_label.configure(image=im2)
         count += 1
+        # when it reaches the end - starts again from beggining
         if count == self.frames:
             count = 0
         anim = self.after(50,lambda :self.animation(count))
